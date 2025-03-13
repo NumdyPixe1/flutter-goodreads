c284ce7089abdce65ebfd20abf9dc61a0e7bac1a
NumdyPixe1 <numdao123@gmail.com>
2025-03-13 12:36:48 +0700

    update mybooks

--- lib/home/widgets/tabs/mybooks_widget.dart
+++ lib/home/widgets/tabs/mybooks_widget.dart
@@ -10,29 +10,60 @@ class MybooksWidget extends GetView {
       backgroundColor: Color.fromRGBO(27, 27, 27, 1),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('shelves'.toUpperCase(),
-                style: Theme.of(context)
-                    .textTheme
-                    .titleMedium
-                    ?.copyWith(color: Colors.white)),
+            SizedBox(
+              height: 24,
+            ),
+            _textButtonItem(context, 'shelves'),
+            SizedBox(
+              height: 24,
+            ),
             _elevatedButtonItem(context, 'Update yoyr reading progress'),
+            // ListView(
+            //   children: [
+            //     ListTile(
+            //       title: Text('Read'),
+            //       subtitle: Text('23 books'),
+            //     )
+            //   ],
+            // ),
             Divider(),
+            SizedBox(
+              height: 10,
+            ),
             TextButton(
                 onPressed: () {},
                 child: Text('See all'.toUpperCase(),
                     style: Theme.of(context).textTheme.titleMedium?.copyWith(
                         color: Color.fromRGBO(60, 143, 132, 1),
                         fontWeight: FontWeight.bold))),
+            SizedBox(
+              height: 10,
+            ),
             Divider(),
-            Text('tags'.toUpperCase(),
-                style: Theme.of(context)
-                    .textTheme
-                    .titleMedium
-                    ?.copyWith(color: Colors.white)),
-            Divider(),
+            SizedBox(
+              height: 10,
+            ),
+            _textButtonItem(context, 'tags'),
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _tagButtonItem(context, 'history-of-southeast-asia', '(5)'),
+                SizedBox(
+                  width: 6,
+                  height: 6,
+                ),
+                _tagButtonItem(context, 'manga', '(8)'),
+              ],
+            ),
+            SizedBox(
+              height: 24,
+            ),
             TextButton(
                 onPressed: () {},
                 child: Text('See all'.toUpperCase(),
@@ -42,11 +73,69 @@ class MybooksWidget extends GetView {
             Divider(
               color: Color.fromRGBO(255, 255, 255, 0.50),
             ),
+            SizedBox(
+              height: 24,
+            ),
             _elevatedButtonItem(context, '+ Create a new tag or shelf'),
+            SizedBox(
+              height: 24,
+            ),
+            Column(
+              children: [
+                Text('reading activity'.toUpperCase(),
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(color: Colors.white)),
+
+                Divider(),
+                // Container(
+                //   width: 180,
+                //   height: 2,
+                //   color: Colors.grey,
+                // ),
+              ],
+            ),
           ],
         ),
       ),
     );
+  }
+
+  TextButton _tagButtonItem(
+      BuildContext context, String titleButton, String numTag) {
+    return TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: Color.fromRGBO(60, 143, 132, 0.5)),
+        onPressed: () {},
+        child: Text(
+          titleButton + ' ' + numTag,
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+              color: Color.fromRGBO(60, 143, 132, 1),
+              fontWeight: FontWeight.bold),
+        ));
+  }
+
+  TextButton _textButtonItem(BuildContext context, String titleButton) {
+    return TextButton(
+        onPressed: () {},
+        child: Column(
+          children: [
+            Text(titleButton.toUpperCase(),
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium
+                    ?.copyWith(color: Colors.white)),
+            SizedBox(
+              height: 4,
+            ),
+            Container(
+              width: 180,
+              height: 2,
+              color: Colors.grey,
+            ),
+          ],
+        ));
   }
 
   ElevatedButton _elevatedButtonItem(BuildContext context, String titleButton) {
--- lib/home/widgets/tabs/search_widget.dart
+++ lib/home/widgets/tabs/search_widget.dart
@@ -13,6 +13,10 @@ class SearchWidget extends GetView {
           padding: const EdgeInsets.all(18.0),
           child: Column(
             children: [
+              _textButtonItem(context, 'expolre by genre'),
+              SizedBox(
+                height: 10,
+              ),
               Text(
                 'See new releases, most-read books, quotes, lists, and more in these popular genres.',
                 style: Theme.of(context)
@@ -20,11 +24,36 @@ class SearchWidget extends GetView {
                     .titleMedium
                     ?.copyWith(color: Colors.white),
                 textAlign: TextAlign.center,
-              )
+              ),
+              SizedBox(
+                height: 10,
+              ),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+TextButton _textButtonItem(BuildContext context, String titleButton) {
+  return TextButton(
+      onPressed: () {},
+      child: Column(
+        children: [
+          Text(titleButton.toUpperCase(),
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium
+                  ?.copyWith(color: Colors.white)),
+          SizedBox(
+            height: 4,
+          ),
+          Container(
+            width: 180,
+            height: 2,
+            color: Colors.grey,
+          ),
+        ],
+      ));
 }
